b2270d0c27df40c889b138a412e1c9c556ca8cbb
denis <denisbelyavski2001@gmail.com>
2025-06-30 17:55:39 +0300

    1323

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList <Integer> count = new ArrayList<>() ;
+        
+        count.add(222);
+        count.add(333);
+        count.add(444);
+        count.add(222);
+        count.add(555);
+
+        List<Integer> collect = count.stream()
+
+                .filter(n -> n % 2 == 0)
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
+
         }
     }
-}
